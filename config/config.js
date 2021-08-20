504431e9c172a3999dc160cb361dcad9d8e6b849
creatorclark <digitalcraftsca@gmail.com>
2021-08-20 01:41:10 -0400

    config

--- config/config.js
+++ config/config.js
@@ -2,18 +2,11 @@
 
 module.exports = {
   "production": {
-    "username": "sunktzblwqxtzc",
-    "password": "8722106cc8e04cee8898ccc205ec1144bec6110c39a77913ee6ff3e9e66926c2",
-    "database": "d2e2ohdpssmk3",
-    "host": "ec2-34-204-128-77.compute-1.amazonaws.com",
-    "dialect": "postgres",
-    "protocol": "postgres",
-    "dialectOptions": {
-      "ssl": {
-        "require": true,
-        "rejectUnauthorized": false 
-      }
-    }
+    "username": "",
+    "password": null,
+    "database": "yelpdb",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
   },
 
   "test": {
