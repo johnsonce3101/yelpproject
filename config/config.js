e8ee0c33a686061ee33771ff3721fe4b714fdd06
creatorclark <digitalcraftsca@gmail.com>
2021-08-20 00:45:04 -0400

    changed production

--- config/config.js
+++ config/config.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "username": "",
-    "password": null,
-    "database": "yelpdb",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+    "protocol": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false 
+      }
+    }
   },
 
   "test": {
