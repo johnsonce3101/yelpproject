f63667305b80797e330bf4456ca226f707660607
creatorclark <digitalcraftsca@gmail.com>
2021-08-19 23:47:39 -0400

    testing routes in index

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,43 @@ router.get('/', async (req, res) => {
     })
 });
 
+router.get('/users/register', (req, res) => {
+    res.render('register')
+});
+
+router.get('users/login', (req, res) => {
+    res.render('login')
+});
+
+router.post('/register', async (req, res) => {
+    let{name, email, userName, password} = req.body
+        const newUser = await Users.find({
+            where: {
+                name: !name,
+                email: !email,
+                userName: !userName
+            },
+        });
+        if (!newUser) {
+            res.send(`${userName} already exists. please try again`)
+        }
+        else async (newUser) => {
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            const createUser = await Users.create({
+                name: name,
+                email: email,
+                userName: userName,
+                password: hashedPassword
+            });
+            res.redirect(`/login`);
+            
+        };
+    });
+
+
+// router.post('/register', () => {
+//     console.log('works')
+// });
+
 module.exports = router;
