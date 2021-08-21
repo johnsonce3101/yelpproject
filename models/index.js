d4d2dd3169d956699cfd8fc1bdaed501627c7e42
creatorclark <digitalcraftsca@gmail.com>
2021-08-21 00:39:24 -0400

    config

--- models/index.js
+++ models/index.js
@@ -1,24 +1,22 @@
 'use strict';
-//import the filesystem module
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-//function to get filenames in dir with ext
 const basename = path.basename(__filename);
-//use login information contained in "development"
-const env = process.env.NODE_ENV || 'production';
-const config = require(__dirname + '/../config/config.js')[env];
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.userName, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
-  .readdirSync(__dirname) //sync read contents of a dir (path)
+  .readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
@@ -37,4 +35,3 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
-//
