62fb9e985730f39aacff214c300ad77f17c02240
Christopher Johnson <johnsonce@goldmail.etsu.edu>
2021-08-30 16:11:57 -0400

    update

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": "",
-    "password": null,
-    "database": "yelpdb",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
 
