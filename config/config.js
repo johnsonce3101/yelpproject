9cd38cb29ba5b01da92bfe7bf8948346f56d97ae
Christopher Johnson <johnsonce@goldmail.etsu.edu>
2021-08-30 18:03:16 -0400

    more updates

--- config/config.js
+++ config/config.js
@@ -4,9 +4,15 @@ module.exports = {
   "development": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
+    "database": process.env.DB_NAME, 
     "host": process.env.DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false 
+      }
+    }
   },
 
   "test": {
